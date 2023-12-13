1d5f6e36df60ad5e8e6a57b0118011759b0bb52c
Sinthooran Ravinathan <srparamu0716@gmail.com>
2023-12-12 21:58:17 -0500

    Updated Next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,24 @@ const nextConfig = {
     API_URL_DEV: process.env.NEXT_PUBLIC_API_URL_DEV,
     API_URL_PROD: process.env.NEXT_PUBLIC_API_URL_PROD,
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              process.env.NODE_ENV === "development"
+                ? "*"
+                : process.env.NEXT_PUBLIC_API_URL_PROD,
+          },
+          { key: "Access-Control-Allow-Headers", value: "*" },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
