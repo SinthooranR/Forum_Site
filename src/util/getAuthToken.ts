25fd58a392745f3d2a850b3d29b2f02766de9c86
Sinthooran Ravinathan <srparamu0716@gmail.com>
2023-12-14 13:52:09 -0500

    Fixed cookie issues and CORs

--- src/util/getAuthToken.ts
+++ src/util/getAuthToken.ts
@@ -1,18 +1,27 @@
 import apiUrl from "@/getApiPath";
 import axios from "axios";
+import { parseCookies } from "nookies";
 
 export const getAuthToken = async (email: string, password: string) => {
   try {
-    const response = await axios.post(`${apiUrl}/apiUser/login`, {
-      email,
-      password,
-    });
+    const response = await axios.post(
+      `${apiUrl}/apiUser/login`,
+      {
+        email,
+        password,
+      },
+      { withCredentials: true }
+    );
 
-    if (!response.data.token) {
+    const cookies = parseCookies({}, response.headers["set-cookie"] || "");
+
+    const token = cookies.token;
+
+    if (!token) {
       throw new Error("Authentication failed");
     }
 
-    return response.data.token;
+    return token;
   } catch (error) {
     console.error("Error fetching token:", error);
     throw error;
