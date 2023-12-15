5013bdf6bfea941a3ff48f104803800aa4418c75
Sinthooran Ravinathan <srparamu0716@gmail.com>
2023-12-14 21:21:20 -0500

    axios credentials

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import { AuthProvider } from "@/util/auth-context";
+import axios from "axios";
 import type { AppProps } from "next/app";
+
+axios.defaults.withCredentials = true;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
